3561970916028b45ac4e3cf2c45a1f44c2341d19
Garriot Zhang <garriot@gmail.com>
2015-09-10 22:43:11 +0800

    support the latest aeson & bson

--- Data/Bson/Json.hs
+++ Data/Bson/Json.hs
@@ -2,200 +2,210 @@
 
 {-# LANGUAGE TypeSynonymInstances, FlexibleInstances, OverloadedStrings #-}
 
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances, OverloadedStrings #-}
+
 module Data.Bson.Json where
 
 import qualified Data.Bson as B
-import qualified Data.Aeson as J
-import qualified Data.Attoparsec.Number as J (Number(..))
-import qualified Data.Vector as V
-import qualified Data.HashMap.Lazy as M
+import qualified Data.Aeson as A
+import qualified Data.Aeson.Types as AT
+import qualified Data.Text.Encoding as E
 import qualified Data.Text as T
-import qualified Data.Text.Encoding as T
-import qualified Data.UString as U
-import qualified Data.ByteString.Char8 as S
-import Control.Monad
-import Control.Applicative
+import qualified Data.HashMap.Lazy as HM
+import qualified Data.Vector as V
+import qualified Data.Scientific as S
+import           Control.Monad
 
-instance J.ToJSON B.Document where
-	toJSON fields = J.object $ map toJSONPair fields
-		where toJSONPair (label B.:= value) = (T.decodeUtf8 $ U.toByteString label, J.toJSON value)
 
-instance J.FromJSON B.Document where
-	parseJSON (J.Object dict) = mapM parseJSONPair $ M.toList dict where
-		parseJSONPair (label, value) = fmap (U.fromByteString_ (T.encodeUtf8 label) B.:=) $ J.parseJSON value
-	parseJSON x = fail $ "BSON document expects to be deriving from a JSON Object, not " ++ show x
+instance A.ToJSON B.Document where
+    toJSON fields = A.object $ map toJSONPair fields
+        where
+            toJSONPair :: B.Field -> AT.Pair
+            toJSONPair (label B.:= value) = (label, A.toJSON value)
 
-instance J.ToJSON B.Value where
-	toJSON v = case v of
-		B.Float double -> J.Number $ J.D double
-		B.String string -> J.String $ T.decodeUtf8 $ U.toByteString string
-		B.Doc document -> J.Object obj where J.Object obj = J.toJSON document
-		B.Array values -> J.Array $ V.fromList $ map J.toJSON values
-		B.Bool bool -> J.Bool bool
-		B.Null -> J.Null
-		B.Int32 int -> J.Number $ J.I $ fromIntegral int
-		B.Int64 int -> J.Number $ J.I $ fromIntegral int
-		B.Bin binary -> J.toJSON binary
-		B.Fun function -> J.toJSON function
-		B.Uuid uuid -> J.toJSON uuid
-		B.Md5 md5 -> J.toJSON md5
-		B.UserDef userDefined -> J.toJSON userDefined
-		B.ObjId objectId -> J.toJSON objectId
-		B.UTC utcTime -> J.toJSON utcTime
-		B.RegEx regex -> J.toJSON regex
-		B.JavaScr javascript -> J.toJSON javascript
-		B.Sym symbol -> J.toJSON symbol
-		B.Stamp mongoStamp -> J.toJSON mongoStamp
-		B.MinMax minMaxKey -> J.toJSON minMaxKey
+instance A.ToJSON B.Value where
+    toJSON val = case val of
+        B.Float double -> A.toJSON double
+        B.String string -> A.toJSON string
+        B.Doc doc -> A.toJSON doc
+        B.Array array -> A.toJSON array
+        B.Bin binary -> A.toJSON binary
+        B.Fun function -> A.toJSON function
+        B.Uuid uuid -> A.toJSON uuid
+        B.Md5 md5 -> A.toJSON md5
+        B.UserDef userDef -> A.toJSON userDef
+        B.ObjId objId -> A.toJSON objId
+        B.Bool bool -> A.toJSON bool
+        B.UTC utc -> A.toJSON utc
+        B.Null -> A.toJSON A.Null
+        B.RegEx regex -> A.toJSON regex
+        B.JavaScr js -> A.toJSON js
+        B.Sym sym -> A.toJSON sym
+        B.Int32 int32 -> A.toJSON int32
+        B.Int64 int64 -> A.toJSON int64
+        B.Stamp stamp -> A.toJSON stamp
+        B.MinMax minMaxKey -> A.toJSON minMaxKey
 
-instance J.FromJSON B.Value where
-	parseJSON v = case v of
-		J.Object _ -> msum [
-			B.Bin <$> J.parseJSON v,
-			B.Fun <$> J.parseJSON v,
-			B.Uuid <$> J.parseJSON v,
-			B.Md5 <$> J.parseJSON v,
-			B.UserDef <$> J.parseJSON v,
-			B.ObjId <$> J.parseJSON v,
-			B.RegEx <$> J.parseJSON v,
-			B.JavaScr <$> J.parseJSON v,
-			B.Sym <$> J.parseJSON v,
-			B.Stamp <$> J.parseJSON v,
-			B.MinMax <$> J.parseJSON v,
-			B.Doc <$> J.parseJSON v ]
-		J.Array vec -> B.Array <$> mapM J.parseJSON (V.toList vec)
-		J.String text -> pure $ case J.fromJSON v of
-			J.Success utcTime -> B.UTC utcTime
-			J.Error _ -> B.String $ U.fromByteString_ $ T.encodeUtf8 text
-		J.Number num -> pure $ case num of
-			J.I int -> B.val int
-			J.D double -> B.Float double
-		J.Bool bool -> pure $ B.Bool bool
-		J.Null -> pure B.Null
+instance A.ToJSON B.Binary where
+    toJSON (B.Binary bs) = A.object [("#_BSON_Binary" , A.toJSON $ E.decodeUtf8 bs)]
 
-instance J.ToJSON B.Binary where
-	toJSON (B.Binary byteString) = J.object [("#_BSON_Binary", J.String $ T.pack $ S.unpack byteString)]
+instance A.ToJSON B.Function where
+    toJSON (B.Function bs) = A.object [("#_BSON_Function" , A.toJSON $ E.decodeUtf8 bs)]
 
-instance J.FromJSON B.Binary where
-	parseJSON v = go `mplus` fail' where
-		go = do
-			J.Object dict <- return v
-			[("#_BSON_Binary", J.String text)] <- return $ M.toList dict
-			return $ B.Binary $ S.pack $ T.unpack text
-		fail' = fail $ "BSON Binary expects object with special field #_BSON_Binary, not " ++ show v
+instance A.ToJSON B.UUID where
+    toJSON (B.UUID bs) = A.object [("#_BSON_UUID" , A.toJSON $ E.decodeUtf8 bs)]
 
-instance J.ToJSON B.Function where
-	toJSON (B.Function byteString) = J.object [("#_BSON_Function", J.String $ T.pack $ S.unpack byteString)]
+instance A.ToJSON B.MD5 where
+    toJSON (B.MD5 bs) = A.object [("#_BSON_MD5" , A.toJSON $ E.decodeUtf8 bs)]
 
-instance J.FromJSON B.Function where
-	parseJSON v = go `mplus` fail' where
-		go = do
-			J.Object dict <- return v
-			[("#_BSON_Function", J.String text)] <- return $ M.toList dict
-			return $ B.Function $ S.pack $ T.unpack text
-		fail' = fail $ "BSON Function expects object with special field #_BSON_Function, not " ++ show v
+instance A.ToJSON B.UserDefined where
+    toJSON (B.UserDefined bs) = A.object [("#_BSON_UserDefined" , A.toJSON $ E.decodeUtf8 bs)]
 
-instance J.ToJSON B.UUID where
-	toJSON (B.UUID byteString) = J.object [("#_BSON_UUID", J.String $ T.pack $ S.unpack byteString)]
+instance A.ToJSON B.ObjectId where
+    toJSON oid = A.object [("#_BSON_ObjectId" , A.String $! T.pack $ show oid)]
 
-instance J.FromJSON B.UUID where
-	parseJSON v = go `mplus` fail' where
-		go = do
-			J.Object dict <- return v
-			[("#_BSON_UUID", J.String text)] <- return $ M.toList dict
-			return $ B.UUID $ S.pack $ T.unpack text
-		fail' = fail $ "BSON UUID expects object with special field #_BSON_UUID, not " ++ show v
+instance A.ToJSON B.Regex where
+    toJSON (B.Regex regex options) = A.object [("#_BSON_Regex" , A.object [
+              ("pattern", A.toJSON regex)
+            , ("options", A.toJSON options)
+        ])]
 
-instance J.ToJSON B.MD5 where
-	toJSON (B.MD5 byteString) = J.object [("#_BSON_MD5", J.String $ T.pack $ S.unpack byteString)]
+instance A.ToJSON B.Javascript where
+    toJSON (B.Javascript env code) = A.object [("#_BSON_Javascript" , A.object [
+              ("enviroment", A.toJSON env)
+            , ("code", A.toJSON code)
+        ])]
 
-instance J.FromJSON B.MD5 where
-	parseJSON v = go `mplus` fail' where
-		go = do
-			J.Object dict <- return v
-			[("#_BSON_MD5", J.String text)] <- return $ M.toList dict
-			return $ B.MD5 $ S.pack $ T.unpack text
-		fail' = fail $ "BSON MD5 expects object with special field #_BSON_MD5, not " ++ show v
+instance A.ToJSON B.Symbol where
+    toJSON (B.Symbol sym) = A.object [("#_BSON_Symbol" , A.toJSON sym)]
 
-instance J.ToJSON B.UserDefined where
-	toJSON (B.UserDefined byteString) = J.object [("#_BSON_UserDefined", J.String $ T.pack $ S.unpack byteString)]
+instance A.ToJSON B.MongoStamp where
+    toJSON (B.MongoStamp stamp) = A.object [ ("#_BSON_MongoStamp", A.toJSON stamp)] 
 
-instance J.FromJSON B.UserDefined where
-	parseJSON v = go `mplus` fail' where
-		go = do
-			J.Object dict <- return v
-			[("#_BSON_UserDefined", J.String text)] <- return $ M.toList dict
-			return $ B.UserDefined $ S.pack $ T.unpack text
-		fail' = fail $ "BSON UserDefined expects object with special field #_BSON_UserDefined, not " ++ show v
+instance A.ToJSON B.MinMaxKey where
+    toJSON x = A.object [("#_BSON_MinMaxKey", A.String $ case x of B.MinKey -> "MinKey"; B.MaxKey -> "MaxKey")]
 
-instance J.ToJSON B.Regex where
-	toJSON (B.Regex pattern options) = J.object [("#_BSON_Regex", J.object [
-		("pattern", J.String $ T.decodeUtf8 $ U.toByteString pattern),
-		("options", J.String $ T.decodeUtf8 $ U.toByteString options)] )]
+instance A.FromJSON B.Document where
+    parseJSON (A.Object dict) = mapM parseJSONPair $ HM.toList dict
+        where
+            parseJSONPair (label, value)= fmap (label B.:=) $ A.parseJSON value
+    parseJSON x = fail $ "BSON document expects to be deriving from a JSON Object, not " ++ show x
 
-instance J.FromJSON B.Regex where
-	parseJSON v = go `mplus` fail' where
-		go = do
-			J.Object dict <- return v
-			[("#_BSON_Regex", J.Object dict2)] <- return $ M.toList dict
-			[("pattern", J.String pattern), ("options", J.String options)] <- return $ M.toList dict2
-			return $ B.Regex (U.fromByteString_ $ T.encodeUtf8 pattern) (U.fromByteString_ $ T.encodeUtf8 options)
-		fail' = fail $ "BSON Regex expects object with special field #_BSON_Regex, not " ++ show v
+instance A.FromJSON B.Value where
+    parseJSON v = case v of
+        AT.Object _ -> msum [
+            B.Bin <$> A.parseJSON v,
+            B.Fun <$> A.parseJSON v,
+            B.Uuid <$> A.parseJSON v,
+            B.Md5 <$> A.parseJSON v,
+            B.UserDef <$> A.parseJSON v,
+            B.ObjId <$> A.parseJSON v,
+            B.RegEx <$> A.parseJSON v,
+            B.JavaScr <$> A.parseJSON v,
+            B.Sym <$> A.parseJSON v,
+            B.Stamp <$> A.parseJSON v,
+            B.MinMax <$> A.parseJSON v,
+            B.Doc <$> A.parseJSON v ]
+        A.Array vec -> B.Array <$> mapM A.parseJSON (V.toList vec)
+        A.String text -> pure $ case A.fromJSON v of
+            A.Success utcTime -> B.UTC utcTime
+            A.Error _ -> B.String text
+        A.Number num -> pure $ case S.floatingOrInteger num of
+            Left float -> B.Float float
+            Right int -> B.val (int::Int)
+        A.Bool bool -> pure $ B.Bool bool
+        A.Null -> pure B.Null
 
-instance J.ToJSON B.Javascript where
-	toJSON (B.Javascript environment code) = J.object [("#_BSON_Javascript", J.object [
-		("environment", J.toJSON environment),
-		("code", J.String $ T.decodeUtf8 $ U.toByteString code)] )]
 
-instance J.FromJSON B.Javascript where
-	parseJSON v = go `mplus` fail' where
-		go = do
-			J.Object dict <- return v
-			[("#_BSON_Javascript", J.Object dict2)] <- return $ M.toList dict
-			[("environment", J.Object env), ("code", J.String code)] <- return $ M.toList dict2
-			flip B.Javascript (U.fromByteString_ $ T.encodeUtf8 code) <$> J.parseJSON (J.Object env)
-		fail' = fail $ "BSON Javascript expects object with special field #_BSON_Javascript, not " ++ show v
+instance A.FromJSON B.Binary where
+    parseJSON v = go `mplus` fail' where
+        go = do
+            A.Object dict <- return v
+            [("#_BSON_Binary", A.String text)] <- return $ HM.toList dict
+            return $ B.Binary $ E.encodeUtf8 text
+        fail' = fail $ "BSON Binary expects object with special field #_BSON_Binary, not " ++ show v
 
-instance J.ToJSON B.Symbol where
-	toJSON (B.Symbol string) = J.object [("#_BSON_Symbol", J.String $ T.decodeUtf8 $ U.toByteString string)]
+instance A.FromJSON B.Function where
+    parseJSON v = go `mplus` fail' where
+        go = do
+            A.Object dict <- return v
+            [("#_BSON_Function", A.String text)] <- return $ HM.toList dict
+            return $ B.Function $ E.encodeUtf8 text
+        fail' = fail $ "BSON Function expects object with special field #_BSON_Function, not " ++ show v
 
-instance J.FromJSON B.Symbol where
-	parseJSON v = go `mplus` fail' where
-		go = do
-			J.Object dict <- return v
-			[("#_BSON_MD5", J.String text)] <- return $ M.toList dict
-			return $ B.Symbol $ U.fromByteString_ $ T.encodeUtf8 text
-		fail' = fail $ "BSON Symbol expects object with special field #_BSON_Symbol, not " ++ show v
+instance A.FromJSON B.UUID where
+    parseJSON v = go `mplus` fail' where
+        go = do
+            A.Object dict <- return v
+            [("#_BSON_UUID", A.String text)] <- return $ HM.toList dict
+            return $ B.UUID $ E.encodeUtf8 text
+        fail' = fail $ "BSON UUID expects object with special field #_BSON_UUID, not " ++ show v
 
-instance J.ToJSON B.MongoStamp where
-	toJSON (B.MongoStamp int) = J.object [("#_BSON_MongoStamp", J.Number $ J.I $ fromIntegral int)]
+instance A.FromJSON B.MD5 where
+    parseJSON v = go `mplus` fail' where
+        go = do
+            A.Object dict <- return v
+            [("#_BSON_MD5", A.String text)] <- return $ HM.toList dict
+            return $ B.MD5 $ E.encodeUtf8 text
+        fail' = fail $ "BSON MD5 expects object with special field #_BSON_MD5, not " ++ show v
 
-instance J.FromJSON B.MongoStamp where
-	parseJSON v = go `mplus` fail' where
-		go = do
-			J.Object dict <- return v
-			[("#_BSON_MongoStamp", J.Number (J.I int))] <- return $ M.toList dict
-			return $ B.MongoStamp $ fromIntegral int
-		fail' = fail $ "BSON MongoStamp expects object with special field #_BSON_MongoStamp, not " ++ show v
+instance A.FromJSON B.UserDefined where
+    parseJSON v = go `mplus` fail' where
+        go = do
+            A.Object dict <- return v
+            [("#_BSON_UserDefined", A.String text)] <- return $ HM.toList dict
+            return $ B.UserDefined $ E.encodeUtf8 text
+        fail' = fail $ "BSON UserDefined expects object with special field #_BSON_UserDefined, not " ++ show v
 
-instance J.ToJSON B.MinMaxKey where
-	toJSON x = J.object [("#_BSON_MinMaxKey", J.String $ case x of B.MinKey -> "MinKey"; B.MaxKey -> "MaxKey")]
+instance A.FromJSON B.ObjectId where
+    parseJSON v = go `mplus` fail' where
+        go = do
+            A.Object dict <- return v
+            [("#_BSON_ObjectId", A.String text)] <- return $ HM.toList dict
+            return $ read $ T.unpack text
+        fail' = fail $ "BSON ObjectId expects object with special field #_BSON_ObjectId, not " ++ show v
 
-instance J.FromJSON B.MinMaxKey where
-	parseJSON v = go `mplus` fail' where
-		go = do
-			J.Object dict <- return v
-			[("#_BSON_MinMaxKey", J.String text)] <- return $ M.toList dict
-			case text of "MinKey" -> return B.MinKey; "MaxKey" -> return B.MaxKey; _ -> mzero
-		fail' = fail $ "BSON MinMaxKey expects object with special field #_BSON_MinMaxKey, not " ++ show v
+instance A.FromJSON B.Regex where
+    parseJSON v = go `mplus` fail' where
+        go = do
+            A.Object dict <- return v
+            [("#_BSON_Regex", A.Object dict2)] <- return $ HM.toList dict
+            [("pattern", A.String pattern), ("options", A.String options)] <- return $ HM.toList dict2
+            return $ B.Regex pattern options
+        fail' = fail $ "BSON Regex expects object with special field #_BSON_Regex, not " ++ show v
 
-instance J.ToJSON B.ObjectId where
-	toJSON x = J.object [("#_BSON_ObjectId", J.String $ T.pack $ show x)]
+instance A.FromJSON B.Javascript where
+    parseJSON v = go `mplus` fail' where
+        go = do
+            A.Object dict <- return v
+            [("#_BSON_Javascript", A.Object dict2)] <- return $ HM.toList dict
+            [("environment", A.Object env), ("code", A.String code)] <- return $ HM.toList dict2
+            flip B.Javascript code <$> A.parseJSON (A.Object env)
+        fail' = fail $ "BSON Javascript expects object with special field #_BSON_Javascript, not " ++ show v
 
-instance J.FromJSON B.ObjectId where
-	parseJSON v = go `mplus` fail' where
-		go = do
-			J.Object dict <- return v
-			[("#_BSON_ObjectId", J.String text)] <- return $ M.toList dict
-			return $ read $ T.unpack text
-		fail' = fail $ "BSON ObjectId expects object with special field #_BSON_ObjectId, not " ++ show v
+instance A.FromJSON B.Symbol where
+    parseJSON v = go `mplus` fail' where
+        go = do
+            A.Object dict <- return v
+            [("#_BSON_MD5", A.String text)] <- return $ HM.toList dict
+            return $ B.Symbol text
+        fail' = fail $ "BSON Symbol expects object with special field #_BSON_Symbol, not " ++ show v
+
+instance A.FromJSON B.MongoStamp where
+    parseJSON v = go `mplus` fail' where
+        go = do
+            A.Object dict <- return v
+            [("#_BSON_MongoStamp", A.Number int)] <- return $ HM.toList dict
+            mongoStamp int
+        fail' = fail $ "BSON MongoStamp expects object with special field #_BSON_MongoStamp, not " ++ show v
+        mongoStamp int = case S.toBoundedInteger int of 
+            Just x -> return $ B.MongoStamp x
+            Nothing -> fail'
+
+instance A.FromJSON B.MinMaxKey where
+    parseJSON v = go `mplus` fail' where
+        go = do
+            A.Object dict <- return v
+            [("#_BSON_MinMaxKey", A.String text)] <- return $ HM.toList dict
+            case text of "MinKey" -> return B.MinKey; "MaxKey" -> return B.MaxKey; _ -> mzero
+        fail' = fail $ "BSON MinMaxKey expects object with special field #_BSON_MinMaxKey, not " ++ show v
